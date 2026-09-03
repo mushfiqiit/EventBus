80c12fc9a0d8b06e1884bd39f380c7a0fc8d4749
Mushfiqur Rahman Chowdhury <mushfiqurchowdhury05@gmail.com>
2026-09-03 01:43:50 -0400

    Manually modified evaluation

--- speciminout/39_implementation/org/greenrobot/eventbus/android/AndroidComponents.java
+++ speciminout/39_implementation/org/greenrobot/eventbus/android/AndroidComponents.java
@@ -2,5 +2,5 @@ package org.greenrobot.eventbus.android;
 
 public abstract class AndroidComponents {
 
-  private static final AndroidComponents implementation = null;
+  private static final AndroidComponents implementation;
 }
